0fa87fd95b58613b8c71dd8105509390a980ecd7
Joran Schrievers <99062987@mydavinci.nl>
2022-03-04 12:06:48 +0100

    Connected database and made a few querys

---
+++ database.php
@@ -0,0 +1,63 @@
+<?php 
+function connAll(){ 
+	try{
+	$conn = new PDO('mysql:host=localhost;dbname=todolist','root', 'mysql');
+	return $conn;
+}
+	catch(PDOException $e){
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
+	}
+} 
+
+function allTodo(){
+	$conn = connAll();
+	$query = "SELECT * FROM todo ORDER BY id";
+	$conn = connAll();
+	$result = $conn->prepare($query);
+	$result->execute();
+	$rows = $result->fetchAll();
+	return $rows;
+}
+
+function allTask(){
+	$conn = connAll();
+	$query = "SELECT * FROM task ORDER BY id";
+	$conn = connAll();
+	$result = $conn->prepare($query);
+	$result->execute();
+	$rows = $result->fetchAll();
+	return $rows;
+}
+function addTodo($id,$name){
+	$conn = connAll();
+	$query = $conn->prepare("INSERT INTO todo (id, Name) VALUES (:id, :Name)");
+	$query -> execute(array(
+		':id' => $id, 
+		':Name' => $name));
+}
+function addTask($id,$description,$time,$status){
+	$conn = connAll();
+	$query = $conn->prepare("INSERT INTO task (id, description, time, status) VALUES (:id, :description, :time, :status)");
+	$query -> execute(array(
+	':id' => $id,
+	':description' => $description,
+	':time' => time('h:i:s', strtotime($time)),
+	':status' => $status
+));
+}
+function updateTask($id,$description,$time,$status){
+	$conn = connAll();
+	$query = $conn->prepare("UPDATE task SET description=:description, time=:time, status=:status WHERE id=:id");
+	$query->bindParam(':description',$description);
+	$query->bindParam(':time',$time);
+	$query->bindParam(':status',$status);
+	$query->bindParam(':id',$id);
+}
+function deleteTask($id){
+	$conn = connAll();
+	$query = $conn->prepare("DELETE FROM task WHERE id = :id");
+	$query->execute(array(":id" -> $id));
+}
+
+?>
